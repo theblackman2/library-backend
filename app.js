3ce28883cb1ed4f53b5e609dfacee8e950caabd2
theblackman2 <paskasonga@gmail.com>
2022-09-29 16:07:29 +0100

    Feat: added cors policy

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const booksRouter = require("./routes/books.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
