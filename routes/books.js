58e3b9a8377e650003c7bfa25f104c21d36495a0
theblackman2 <paskasonga@gmail.com>
2022-09-29 17:48:52 +0100

    Refactor: orders books

--- routes/books.js
+++ routes/books.js
@@ -2,10 +2,13 @@ const booksRouter = require("express").Router();
 const mysqlConnexion = require("./../databases/mysql.js");
 
 booksRouter.get("/", (req, res) => {
-  mysqlConnexion.query("SELECT * FROM all_books", (err, rows, fileds) => {
-    if (err) throw err;
-    res.send(rows);
-  });
+  mysqlConnexion.query(
+    "SELECT * FROM all_books ORDER BY title",
+    (err, rows, fileds) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
 });
 
 module.exports = booksRouter;
